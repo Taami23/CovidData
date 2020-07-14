71362db027fb10518ece3ede75c6045a4c18fb16
Tamara <tvale.sv@gmail.com>
2020-07-14 19:35:57 -0400

    creacion de layout

---
+++ app/src/main/java/com/example/coviddata/Actividades/regiones.java
@@ -0,0 +1,16 @@
+package com.example.coviddata.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.coviddata.R;
+
+public class regiones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiones);
+    }
+}
