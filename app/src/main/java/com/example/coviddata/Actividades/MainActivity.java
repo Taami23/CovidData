ee3b6575ed35205a3a6181bfa931103b1e063ed5
Matias <matias.poblete1601@alumnos.ubiobio.cl>
2020-07-14 22:38:15 -0400

    Botones para navegar

--- app/src/main/java/com/example/coviddata/Actividades/MainActivity.java
+++ app/src/main/java/com/example/coviddata/Actividades/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.coviddata.R;
 import com.example.coviddata.Servicio.ServicioWeb;
@@ -13,11 +15,38 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private ServicioWeb servicioWeb;
+    private Button dataNacion;
+    private Button dataRegiones;
+    private Button dataRegion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataNacion = findViewById(R.id.nacionales);
+        dataRegiones = findViewById(R.id.regionales);
+        dataRegion = findViewById(R.id.region);
+
+        dataNacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDataNacion();
+            }
+        });
+
+        dataRegiones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDataRegiones();
+            }
+        });
+
+        dataRegion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDataRegion();
+            }
+        });
         Retrofit retrofit = new Retrofit.Builder().baseUrl("http://covid.unnamed-chile.com/api/")
                 .addConverterFactory(GsonConverterFactory.create()).build();
         servicioWeb = retrofit.create(ServicioWeb.class);
